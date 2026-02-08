6c19fbc3cfd16331f2fac1031bca50d34577117a
natsu8411 <anmolgupta0415@gmail.com>
2026-02-08 10:51:25 +0530

    push

--- DAY 18/main.py
+++ DAY 18/main.py
@@ -69,30 +69,30 @@
 
 
 
-# import turtle as t
-# import random
+import turtle as t
+import random
 
-# tim = t.Turtle()
-# t.colormode(255)
+tim = t.Turtle()
+t.colormode(255)
 
-# def random_color():
-#     r = random.randint(0, 255)
-#     g = random.randint(0, 255)
-#     b = random.randint(0, 255)
-#     color = (r, g, b)
-#     return color
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
-# tim.speed("fastest")
+tim.speed("fastest")
 
 
-# def draw_graph(size_of_gap):
-#     for _ in range(int(360 / size_of_gap)):
-#         tim.color(random_color())
-#         tim.circle(100)
-#         tim.setheading(tim.heading() + 10)
+def draw_graph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + 10)
 
-# draw_graph(5)
+draw_graph(5)
 
-# screen = t.Screen()
-# screen.exitonclick()
+screen = t.Screen()
+screen.exitonclick()
 
